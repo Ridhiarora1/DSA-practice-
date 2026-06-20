3f7853ecbef99c7523f545245e0db06a8a4eb510
Riddhi Arora <ariddhi835@gmail.com>
2026-06-20 19:53:55 +0530

    minEatingSpeed

---
+++ binarySearch1.java
@@ -0,0 +1,36 @@
+class Solution {
+
+    public int minEatingSpeed(int[] piles, int h) {
+        int left = 1;
+        int right = 0;
+
+        for(int pile: piles) {
+            right = Math.max(right , pile); 
+        }
+
+        while(left < right){
+            int mid = left + right-left/2;
+
+            if (canFinish(piles , mid , h)){
+                mid = right; 
+            } else {
+                left = mid + 1; 
+            } 
+        }
+        return left; 
+    } 
+
+    private boolean canFinish(int[] piles , int k , int h) {
+        long hoursNeeded = 0;
+
+        for(int pile : piles){
+            hoursNeeded += (pile - k + 1)/k; 
+
+            if(hoursNeeded > h) return false; 
+        }
+
+        return (hoursNeeded <= h); 
+    }
+} 
+
+
